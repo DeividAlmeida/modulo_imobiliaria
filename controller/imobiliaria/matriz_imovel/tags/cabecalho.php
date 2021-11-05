35176fed1bc98046a06c2fb6a6f26c3baad7bbe6
DeividAlmeida <mr.davialmeida@hotmail.com>
2021-11-05 15:21:46 -0300

    social midias

--- controller/imobiliaria/matriz_imovel/tags/cabecalho.php
+++ controller/imobiliaria/matriz_imovel/tags/cabecalho.php
@@ -271,7 +271,7 @@ ob_start();
 
     <p class="shop--imovel-page--header__resume "style="display:none"><?php echo $imovel['resumo']; ?></p>
 
-    <div class="shop--imovel-page__header--share__wrapper hidden-print" style="display:none">
+    <div class="shop--imovel-page__header--share__wrapper hidden-print" >
       <!-- Sharingbutton Facebook -->
       <a class="shop--imovel-page__header--share__link" href="https://facebook.com/sharer/sharer.php?u=<?php echo urlencode($url); ?>" target="_blank" rel="noopener" aria-label="Facebook" onClick="window.open(this.href,'targetWindow','toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=250'); return false;">
         <div class="shop--imovel-page__header--share shop--imovel-page__header--share--facebook shop--imovel-page__header--share--medium"><div aria-hidden="true" class="shop--imovel-page__header--share__icon shop--imovel-page__header--share__icon--solid">
@@ -296,9 +296,7 @@ ob_start();
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z"/></svg></div>WhatsApp</div>
       </a>
 
-      <a class="shop--imovel-page__header--share__link"  aria-label="Imprimir" id="printBtn" href="javascript:window.print()">
-        <div class="shop--imovel-page__header--share shop--imovel-page__header--share--email shop--imovel-page__header--share--medium">Imprimir</div>
-      </a>
+      
 </div>
 <?php
 $cabecalho  = ob_get_clean();
